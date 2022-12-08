e8cf613a57fdf991c4d2425c8015d4617f3354fd
fernandogiroto <giroto.luis@gmail.com>
2022-12-08 18:14:27 +0100

    pokevue

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+import Habilidades from '@/views/Habilidades'
+import Sobre from '@/views/Sobre'
+import Status from '@/views/Status'
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Home,
+    children: [
+      { path: 'habilidades', component: Habilidades },
+      { path: 'sobre', component: Sobre},
+      { path: 'status', component: Status }
+    ]
   }
 ]
 
